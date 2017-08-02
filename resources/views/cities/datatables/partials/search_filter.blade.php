da281b69e1c6c1df9f360dbe4b577a31c63af216
Konstantin Konstantinov <konstantin.konstantinov@bgosoftware.com>
2017-08-02 15:56:04 +0300

    develop

--- resources/views/cities/datatables/partials/search_filter.blade.php
+++ resources/views/cities/datatables/partials/search_filter.blade.php
@@ -4,10 +4,6 @@
     </div>
     <div class="panel-body">
         <form method="POST" id="search-form" class="form-inline" role="form">
-            {{--<div class="form-group">--}}
-                {{--<label for="search_fips">Fips</label>--}}
-                {{--<input type="text" class="form-control" name="search_fips" id="search_fips" placeholder="search fips">--}}
-            {{--</div>--}}
             <div class="form-group">
                 <label for="search_name">Name</label>
                 <input type="text" class="form-control" name="search_name" id="search_name" placeholder="city name">
